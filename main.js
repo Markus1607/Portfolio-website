10e5780f1de052fb751712d6e099e0b380b09e0c
Mark Sarpong <markus20@outlook.com>
2020-02-27 16:56:10 +0000

    Update bio

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ $(function() {
   //autoplay video
   $("#myVideo")[0].autoplay = true
 
+  if ($("#myVideo")[0].play) {
+    location.reload()
+  }
+
   //This is smooth scrolling to divs
   $("a[href^='#']").on("click", function(e) {
     $("html, body").animate(
